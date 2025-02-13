91cec1be9c26d039d53fd5a23c76c6b6074651b1
JOANNE CHING YIN XUAN <148308964+JOANNE-321@users.noreply.github.com>
2025-02-13 10:57:48 +0800

    Update active-members.php

--- app/views/admins/active-members.php
+++ app/views/admins/active-members.php
@@ -978,6 +978,5 @@ function debugStatuses() {
 <!-- Required JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
 </body>
 </html> 
